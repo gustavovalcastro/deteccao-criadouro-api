01d4c74a4dee4b356a18eabce715d04cc40d20c5
Gustavo Castro <45861294+GustavoVCastro@users.noreply.github.com>
2025-12-01 10:55:30 -0300

    fixing issues with CORS

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI        
 from app.routers import routers   
-from app.database import Base, engine  
+from app.database import Base, engine
+from fastapi.middleware.cors import CORSMiddleware
 
 Base.metadata.create_all(bind=engine) 
 
@@ -10,6 +11,14 @@ app = FastAPI(
     version="1.0.0",
     docs_url="/swagger",   
     redoc_url=None                        
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 for router in routers:
